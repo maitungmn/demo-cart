f45e50c463fa2bdb935451971937a9830c24ddcd
crazycakiem <crazycakiem@gmail.com>
2018-12-02 20:48:02 +0700

    update front end

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,63 @@
 <template>
-  <v-app>
-    <nuxt></nuxt>
-  </v-app>
+ <v-card flat>
+    <v-toolbar
+      dark
+      extended
+      flat
+      class="toolbar-pri"
+    >
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+    </v-toolbar>
+
+    <v-layout row pb-2>
+      <v-flex xs10 offset-xs1>
+        <v-card class="card--flex-toolbar">
+          <v-toolbar card prominent>
+            <v-toolbar-title class="title black--text font-weight-light over-show">El-Meteo - Shopping Cart</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+              <v-icon>search</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>apps</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+          </v-toolbar>
+
+          <v-divider></v-divider>
+
+          <v-app>
+            <nuxt></nuxt>
+          </v-app>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
   export default {}
 </script>
+<style lang="scss">
+ .toolbar-pri{
+  &.theme--dark{
+   &.v-toolbar{
+    background-color: #6394F8;
+   }
+  }
+ }
+ .over-show{
+  &.title{
+   overflow: visible;
+  }
+ }
+ .card--flex-toolbar {
+   margin-top: -64px;
+ }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,212 @@
 <template>
   <div id="app">
-    <div id="product">
+   <v-container grid-list-lg>
+            <v-layout row wrap>
+                <v-flex xs8 class="m-col-l pr-3">
+                    <div class="top-info">
+                        <h4 class="headline font-weight-light">List items</h4>
+                    </div>
+                    <div id="product" class="mt-3">
+                     <v-layout justify-center row wrap>
+                       <v-flex xs4 v-for="(item, index) in items" :key="index">
+                         <v-card class="item-card mate-card1">
+                           <v-img
+                             :src="item.img"
+                           ></v-img>
+                           <div class="box-ctn-detail">
+                            <h1 class="text-xs-left">{{item.title}}</h1>
+                            <h3 class="text-xs-left">
+                              $ {{item.price}}
+                            </h3>
+                            <v-btn fab dark class="btn-add">
+                              <v-icon dark @click="addItem(index)">add</v-icon>
+                            </v-btn>
+                           </div>
+                         </v-card>
+                       </v-flex>
+                     </v-layout>
+                    </div>
+                </v-flex>
+                <v-flex xs4 class="m-col-r pl-3">
+                 <div class="top-info">
+                     <h4 class="headline font-weight-light">Shopping Cart</h4>
+                 </div>
+                 <div class="table-price mt-3">
+                  <v-layout justify-center row wrap>
+                    <v-flex xs12>
+                     <div class="mate-card1">
+                      <v-toolbar class="toolbar-cart pb-4" flat color="white">
+                        <v-icon medium white>shopping_cart</v-icon>
+                        <v-chip small style="background-color:#6394F8" text-color="white">3</v-chip>
+                        <v-spacer></v-spacer>
+                        <div class="text-xs-right"><span style="color:#ABB0BE;">Total:</span> <span style="color:#6394F8;">$ {{syncTotal}}</span></div>
+                      </v-toolbar>
+                      <v-data-table
+                        :items="buyitems"
+                        hide-actions
+                        hide-headers
+                        class="box-cart-checkout"
+                      >
+                        <template slot="items" slot-scope="props" v-if="props.item.click">
+                          <td class="text-xs-left">
+                           <div class="item-short-view">
+                            <v-img class="img-thumb" src="https://chenyiya.com/codepen/product-1.jpg"></v-img>
+                            <v-card class="info-item-ic">
+                             <v-card-title class="item-name">
+                              {{ props.item.title }}
+                              <v-spacer></v-spacer>
+                              <v-tooltip bottom>
+                                <v-icon
+                                  slot="activator"
+                                  small
+                                  @click="hideItem(props.index)">delete
+                                </v-icon>
+                                <span>Remove</span>
+                              </v-tooltip>
+                             </v-card-title>
+                             <v-card-title class="item-price">$ {{ props.item.price }}</v-card-title>
+                             <v-card-actions>
+                              <div class="item-quantity">
+                               <span style="color:#ABB0BE;">Quantity : </span>
+                               <v-tooltip bottom>
+                                 <v-icon
+                                   slot="activator"
+                                   small
+                                   outline
+                                   class="mr-1"
+                                   @click="addItem(props.index)">add
+                                 </v-icon>
+                                 <span>Increase</span>
+                               </v-tooltip>
+                               {{ props.item.click }}
+                               <v-tooltip bottom>
+                                 <v-icon
+                                   slot="activator"
+                                   small
+                                   outline
+                                   class="ml-1"
+                                   @click="removeItem(props.index)">remove
+                                 </v-icon>
+                                 <span>Decrease</span>
+                               </v-tooltip>
+                              </div>
+                              <div class="item-price-total">$ {{ props.item.click * props.item.price }}</div>
+                             </v-card-actions>
+                            </v-card>
+                           </div>
+                          </td>
+                        </template>
+                        <template slot="footer">
+                          <td class="text-xs-center" :colspan="headers.length">
+                            <div class="box-foo"><v-btn block depressed class="checkout-btn">Checkout</v-btn></div>
+                          </td>
+                        </template>
+                      </v-data-table>
+                     </div>
+                    </v-flex>
+                    <v-snackbar
+                      v-model="closeSnack"
+                      :timeout="0"
+                      bottom
+                    >
+                      {{ text }}
+                      <v-btn
+                        color="pink"
+                        flat
+                        @click="applyDiscount"
+                      >
+                        Apply
+                      </v-btn>
+                      <v-btn
+                        color="pink"
+                        flat
+                        @click="closeSnack = false"
+                      >
+                        Close
+                      </v-btn>
+                    </v-snackbar>
+                  </v-layout>
+                 </div>
+                    <!-- <v-card class="total-box bg-light-gray">
+                        <v-card-title>
+                            <span class="subheading">Total</span>
+                            <v-spacer></v-spacer>
+                            <strong class="total-price"><span><span
+                                    class="price-currency">€</span>282.60</span></strong>
+                        </v-card-title>
+                        <v-card-title class="sub-title">
+                            <v-btn :ripple="false" class="show-more" @click="show = !show">
+                                Details
+                                <v-icon>{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
+                            </v-btn>
+                            <v-spacer></v-spacer>
+                            <div class="price-small-text"><span>1 night</span> , <span>2 person</span></div>
+                        </v-card-title>
+                        <v-slide-y-transition>
+                            <v-container class="bill-info-detail" v-show="show">
+                                <v-card-title>
+                                    <span class="price-small-text">Night 1</span>
+                                    <v-spacer></v-spacer>
+                                    <span class="price-small-text"><span><span
+                                            class="price-currency">€</span>212.60</span></span>
+                                </v-card-title>
+                                <v-card-title>
+                                    <strong>Total</strong>
+                                    <v-spacer></v-spacer>
+                                    <strong><span><span
+                                            class="price-currency">€</span>282.60</span></strong>
+                                </v-card-title>
+                                <v-card-title>
+                                    <span class="price-small-text">VAT</span>
+                                    <v-spacer></v-spacer>
+                                    <span class="price-small-text"><span><span
+                                            class="price-currency">€</span>22.60</span></span>
+                                </v-card-title>
+                            </v-container>
+                        </v-slide-y-transition>
+                        <v-divider light></v-divider>
+                        <div class="box-sub-info">
+                            <v-card-title>
+                                <strong>Standard Room</strong>
+                                <v-spacer></v-spacer>
+                                <strong><span><span class="price-currency">€</span>182.60</span></strong>
+                            </v-card-title>
+                            <v-card-title class="sub-title">
+                                <div class="price-small-text">
+                                    Double Room with access to aquapark - City tax for free!<br>
+                                    Bed & Breakfast with free access to Aquapark - Best Available Rate.
+                                </div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-btn flat>Remove</v-btn>
+                            </v-card-actions>
+                        </div>
+                        <div class="box-sub-info">
+                            <v-card-title>
+                                <strong>Standard Room</strong>
+                                <v-spacer></v-spacer>
+                                <strong><span><span class="price-currency">€</span>182.60</span></strong>
+                            </v-card-title>
+                            <v-card-title class="sub-title">
+                                <div class="price-small-text">
+                                    Double Room with access to aquapark - City tax for free!<br>
+                                    Bed & Breakfast with free access to Aquapark - Best Available Rate.
+                                </div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-btn flat>Remove</v-btn>
+                            </v-card-actions>
+                        </div>
+                        <v-divider light></v-divider>
+                        <v-card-actions>
+                            <v-btn large block color="primary" nuxt to="/step2">NEXT</v-btn>
+                        </v-card-actions>
+
+                    </v-card> -->
+                </v-flex>
+            </v-layout>
+        </v-container>
+    <!-- <div id="product">
       <v-container grid-list-md text-xs-center>
         <v-layout justify-center row wrap>
           <v-flex xs4 v-for="(item, index) in items" :key="index">
@@ -19,15 +225,19 @@
           </v-flex>
         </v-layout>
       </v-container>
-    </div>
-    <div class="table-price">
+    </div> -->
+    <!-- <div class="table-price">
       <v-container grid-list-md text-xs-center>
         <v-layout justify-center row wrap>
           <v-flex xs12>
+           <div class="elevation-1">
+            <v-toolbar class="toolbar-cart" flat color="white">
+              <v-toolbar-title>Shopping Cart</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
             <v-data-table
               :headers="headers"
               :items="buyitems"
-              class="elevation-1"
               hide-actions
             >
               <template slot="items" slot-scope="props" v-if="props.item.click">
@@ -76,6 +286,7 @@
                 </td>
               </template>
             </v-data-table>
+           </div>
           </v-flex>
           <v-snackbar
             v-model="closeSnack"
@@ -100,7 +311,7 @@
           </v-snackbar>
         </v-layout>
       </v-container>
-    </div>
+    </div> -->
   </div>
 
 </template>
@@ -128,15 +339,36 @@
             title: "Paper Bag",
             price: "35",
             id: "paper-bag"
+          },
+          {
+            img: "https://chenyiya.com/codepen/product-1.jpg",
+            title: "Beer Bottle 2",
+            price: "55",
+            id: "beer2"
+          },
+          {
+            img: "https://chenyiya.com/codepen/product-2.jpg",
+            title: "Eco Bag 2",
+            price: "71",
+            id: "eco-bag2"
+          },
+          {
+            img: "https://chenyiya.com/codepen/product-3.jpg",
+            title: "Paper Bag 2",
+            price: "32",
+            id: "paper-bag2"
           }
         ],
         buyitems: [
           {click: 0},
           {click: 0},
+          {click: 0},
+          {click: 0},
+          {click: 0},
           {click: 0}
         ],
         headers: [
-          {text: 'Shopping Cart', value: 'title'},
+          {text: 'Item name', value: 'title'},
           {text: 'Price', value: 'price'},
           {text: 'Quantity', value: 'quantity', align: 'center'},
           {text: 'Total', value: 'total', align: 'right'},
@@ -207,6 +439,24 @@
     .table-price {
       max-width: 960px;
       margin: 0 auto;
+      .toolbar-cart{
+        .v-toolbar__content{
+          border-bottom: 0px solid rgba(0, 0, 0, 0.12);
+          background-color: #fff;
+          position: relative;
+          &:after{
+           position: absolute;
+           height: 1px;
+           width: calc(100% - 40px);
+           background-color: rgba(0, 0, 0, 0.12);
+           bottom: 0;
+           left: 0;
+           right: 0;
+           content: '';
+           margin: 0 auto;
+          }
+        }
+      }
       > .container {
         padding-top: 12px;
         .del-box {
@@ -237,24 +487,32 @@
       }
       .item-card {
         position: relative;
-        .btn-add {
-          position: absolute;
-          position: absolute;
-          top: 189px;
-          right: 9px;
-          background-color: #27ae60;
+        &.mate-card1{
+         border: 1px solid #dfe1e5;
         }
-        h1 {
-          margin-left: 20px;
-          padding-top: 30px;
-          font-size: 24px;
-        }
-        h3 {
-          margin-left: 20px;
-          padding-bottom: 20px;
-          padding-top: 15px;
-          font-size: 20px;
-          font-weight: 400;
+        .box-ctn-detail{
+         position: relative;
+         .btn-add {
+           position: absolute;
+           top: -31px;
+           right: 3px;
+           background-color: #6394F8;
+           width: 48px;
+           height: 48px;
+         }
+         h1 {
+           margin-left: 20px;
+           padding-top: 20px;
+           font-size: 18px;
+           font-weight: 500;
+         }
+         h3 {
+           margin-left: 20px;
+           padding-bottom: 20px;
+           padding-top: 5px;
+           font-size: 18px;
+           font-weight: 400;
+         }
         }
       }
       .box {
@@ -395,5 +653,93 @@
       }
     }
   }
+.box-cart-checkout{
+ .v-table{
+  tbody{
+   tr{
+    border-bottom: none !important;
+    td{
+     padding: 0 12px !important;
+     background-color: #FFF !important;
+    }
+   }
+  }
+  tfoot{
+   tr{
+    border-top: none;
+    td{
+     padding: 0 12px;
+     .box-foo{
+      padding-left: 10px;
+      padding-right: 10px;
+     }
+    }
+   }
+  }
+ }
+ .checkout-btn{
+  &.v-btn{
+   background-color: #6394F8 !important;
+   color: #FFF;
+   margin: 25px 0 25px 0;
+   height: 46px;
+   border-radius: 6px;
+  }
+ }
+}
+.mate-card1{
+ background-color: #fff;
+ border: 1px solid #dfe1e5;
+ border-radius: 6px;
+ box-shadow: none;
+ transition: box-shadow .3s linear;
+ overflow: hidden;
+ &:hover{
+  box-shadow: 0 2px 10px 0px rgba(60, 64, 67, .15);
+ }
+}
+.item-short-view{
+ padding-bottom: 5px;
+ margin-bottom: 0;
+ background-color: #FFF;
+ padding-top: 5px;
+ padding-left: 10px;
+ padding-right: 10px;
+ .img-thumb{
+  float: left;
+  width: 70px;
+  height: 70px;
+  position: relative;
+  z-index: 9;
+ }
+ .info-item-ic{
+  padding-top: 0;
+  padding-left: 80px;
+  min-height: 70px;
+  box-shadow: none;
+  .item-name{
+   padding: 0;
+   font-weight: bold;
+  }
+  .item-price{
+   padding: 5px 0 0 0;
+  }
+  .v-card__actions{
+   padding: 5px 10px 10px 0;
+  }
+  .item-quantity{
+   i{
+    border-radius: 30px;
+    border: 1px solid rgba(0,0,0,.12);
+   }
+  }
+  .item-price-total{
+   margin-left: 20px;
+   color: #6394F8;
+  }
+  .item-remove{
 
+  }
+ }
+}
 </style>
